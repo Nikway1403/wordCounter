e80e22f4f36bc5e4ec03a5b295f138574444a240
Nikita <babashov.nikita2@gmail.com>
2024-02-16 15:55:15 +0300

    Version1

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,3 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+namespace WordCounter;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Enter path of input file");
+        var inputFilePath = Console.ReadLine();
+
+        Console.WriteLine("Enter path of output file");
+        var outputFilePath = Console.ReadLine();
+
+        try
+        {
+            if (!File.Exists(inputFilePath))
+            {
+                Console.WriteLine("Input file does't exist");
+                return;
+            }
+
+            Dictionary<string, int> wordCount = CountWords(inputFilePath);
+            WriteCountOfWordsToFile(wordCount, outputFilePath);
+            Console.WriteLine("Words count successfully, see results in file");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+        }
+    }
+
+    static Dictionary<string, int> CountWords(string filePath)
+    {
+        var wordCount = new Dictionary<string, int>();
+
+        using (var reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] words = Regex.Split(line, @"\W+");
+                foreach (string word in words)
+                {
+                    if (!string.IsNullOrWhiteSpace(word))
+                    {
+                        string lowercaseWord = word.ToLower();
+                        if (wordCount.ContainsKey(lowercaseWord))
+                        {
+                            wordCount[lowercaseWord]++;
+                        }
+                        else
+                        {
+                            wordCount[lowercaseWord] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return wordCount;
+    }
+
+    static void WriteCountOfWordsToFile(Dictionary<string, int> wordCount, string outPath)
+    {
+        using (var writer = new StreamWriter(outPath))
+        {
+            foreach (var entry in wordCount.OrderByDescending(pair => pair.Value))
+            {
+                writer.WriteLine($"{entry.Key}\t\t{entry.Value}");
+            }
+        }
+    }
+}
